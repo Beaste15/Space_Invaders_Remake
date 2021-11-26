2fc66d32068fe5e8ea8e220218cc0089a6d97b97
Beaste15 <brycecrafter@bgp.nu>
2021-11-25 19:19:54 -0500

    invaders shoot a lot, score tally

--- Classic_Game_Remake/Assets/Scripts/Player.cs
+++ Classic_Game_Remake/Assets/Scripts/Player.cs
@@ -44,7 +44,7 @@ public class Player : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {     
         if (other.gameObject.layer == LayerMask.NameToLayer("Invader") || other.gameObject.layer == LayerMask.NameToLayer("Missile")) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
